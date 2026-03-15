8d68624fd090f1a965c7b9d2153a5453a3cb3005
harsh666666 <hs006572@gmail.com>
2026-03-15 21:07:51 +0530

    Fixed React warnings for Vercel build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 // frontend/src/App.js
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import axios from "axios";
 import "./App.css";
  function App() {
